f92bc86dd80413977e78cdce22c597dd812b5c40
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-06-25 09:27:02 +0530

    First code

---
+++ day1.py
@@ -0,0 +1,41 @@
+# print("Hello World!")
+"""
+hbcjhx
+xksjbchjs
+cjxbchjns
+bsj
+"""
+
+# print("Hello\t\tRahul \nKaise ho")
+
+# var1_kumar=78
+
+# name=78
+# name=89
+# NAME=56
+# print(name)
+
+# name="Shubham"
+# var1=89
+
+# print(type(name))
+# print(type(var1))
+
+# a=7
+# b=6.7
+# c=int(a+b)
+# print(c)
+# print(type(c))
+
+# var1=int(input("Enter your num1: "))
+# var2=int(input("Enter your num2: "))
+# print(var1+var2)
+# name1="Rohan"
+# name2="Rahul"
+# name3="Suman"
+
+# print(name1,name2,name3,sep="-")
+
+# print(name1,end=" ")
+# print(name2,end=" ")
+# print(name3)
---
+++ day2.py
@@ -0,0 +1,84 @@
+
+# a=10
+# b=3
+# print(a//b)
+# print(a%b)
+# print(a**b)
+
+# print(a>b)
+# print(a>=b)
+# print(a<b)
+# print(a<=b)
+# print(a==b)
+# print(a!=b)
+# a=6
+# # a=a+5
+# a+=5
+# a-=3
+
+# print(a)
+
+# x=12
+# print( x < 3 or x > 7)
+# print( x < 3 and x > 7)
+
+# name="Rohan kaise ho kha ho"
+# name="""
+# hello i am good
+# my name rahul
+# songs
+# """
+
+name="Rohan Kumar"
+# print(name[1])
+# print(name[2:5])
+# print(name[:5])
+# print(name[:])
+# print(name[::2])
+# print(name[-1])
+# print(name[-5:])
+
+# for str1 in name:
+#     print(str1)
+
+# name=input("Enter you name: ")
+# print(name.upper())
+# print(name.lower())
+# trailing="@#$%^&*()"
+
+# print(name.strip())
+
+# print(name.lower())
+
+name="Rohan Kumar"
+# str1="my name is hero"
+# str1="*******end*****"
+# print(name.replace("Kumar","Abhinay"))
+# print(name.replace("Kumar","Abhinay"))
+# print(str1.capitalize())
+# print(str1.center(50))
+# print(name.endswith("Kumary"))
+
+# print(name.find("7"))
+# print(name.index("89"))
+# print("impotant")
+
+# str1 = "WelcomeToTheConsole354sfrdt45t"
+# print(str1.isalnum())
+
+# str1 = "WelcomeToTheConsolet"
+# print(str1.isalpha())
+# print(str1.isprintable())
+# name="        "
+# print(name.isspace())
+
+# str1 = "pytHon is a interpreted Language"
+# # print(str1.swapcase())
+# print(str1.title())
+
+# age=-12
+
+# if(age>18):
+#     print("You can drive")
+# else:
+#     print("You can not drive")
