6d315e714b87fca09e2189454df86502535b9327
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-06-25 10:20:05 +0530

    loops and functions

--- day2.py
+++ day2.py
@@ -82,3 +82,103 @@ name="Rohan Kumar"
 #     print("You can drive")
 # else:
 #     print("You can not drive")
+
+# value=2
+
+# match(value):
+#     case 1:
+#         print("Sunday")
+#     case 2:
+#         print("Monday")
+#     case 3:
+#         print("Tuesday")
+#     case _:
+#         print("Please give value between 1 to 3")
+
+# num1=int(input("Enter num1: "))
+# num2=int(input("Enter num2: "))
+# opt=input("Enter operator: ")
+
+# match(opt):
+#     case "+":
+#         print("The sum is:",(num1+num2))
+#     case "-":
+#         print("The sum is:",(num1-num2))
+#     case _:
+#         print("Please give only + and -")
+
+# value=57
+
+# match(value):
+#     case 1|4|3|5|6|7|8|9|10:
+#         print("You can not drive car")
+#     case 2:
+#         print("You can not drive car")
+#     case 34:
+#         print("You can drive the car")
+#     case 45:
+#         print("You can drive the car")
+#     case _ if value%2==0:
+#         print("This is Even number")
+#     case _ if value%2==1:
+#         print("This is Odd number")
+
+# for i in range(5,20,2):
+#     print(i)
+
+# for i in range(20,10,-1):
+#     print(i)
+
+# i=1
+# while(i<10):
+#     print(i)
+#     i+=1
+
+# num1=int(input("Enter the num: "))
+
+# for i in range(1,11):
+#     if(i==5):
+#         break
+#     print(i)
+
+# for i in range(1,11):
+#     if(i==5 or i==8):
+#         continue
+#     print(i)
+
+# a=6
+# b=5
+# print(a+b)
+
+# a1=3
+# b1=8
+# print(a1+b1)
+
+# def addTwoNum(num1,num2):
+#     print("The sum is:",(num1+num2))
+
+# def greet():
+#     print("Good Morning")
+
+# def factorial(num):
+#     fact=1
+#     for i in range(1,num+1):
+#         fact=fact*i
+#     print("The factorial is:",fact)
+
+
+# addTwoNum(4,7)
+# addTwoNum(7,3)
+
+# userInput=int(input("Enter the number you want factorial: "))
+# factorial(userInput)
+# greet()
+# addTwoNum(4,9)
+# factorial(3)
+
+# def addTwoNum(num1,num2):
+#     # print("The sum is:",(num1+num2))
+#     return num1+num2
+
+
+# print(addTwoNum(6,8))
