6745791c16d93cfde74978784a4d5ff17dbb0233
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-06-29 14:23:01 +0530

    math and os module

---
+++ day4.py
@@ -0,0 +1,70 @@
+
+# d1={
+#     "name":"Rohan",
+#     "age":78,
+#     "city":"Delhi"
+# }
+
+# d1.setdefault("Adhar",4567890)
+
+# d1["Rohan"]="Kumar"
+# print(d1)
+
+# d1={}
+
+# for i in range(2):
+#     key=input("Enter you key: ")
+#     value=input("Enter you name: ")
+#     d1[key]=value
+
+# print(d1)
+
+# age = -5
+# if age < 0:
+#     raise ValueError("Age cannot be negative Meesage daalenge wahi dikhega")
+
+# age=45
+
+# print("Adult") if age>18 else print("Child")
+
+# l1=[4,6,7,8,2,5,8]
+
+# for i in range(len(l1)):
+#     print(i,l1[i])
+
+# for key,value in enumerate(l1,start=5):
+#     print(key,value)
+
+
+
+import math as m
+
+# print(math.factorial(5))
+# print(math.sqrt(25))
+# print(math.cbrt(125))
+
+# print(math.floor(5.67))
+# print(math.ceil(5.67))
+
+# print(math.pow(3,4))
+# print(math.pi)
+
+# print(m.pi)
+# import flask
+# print(dir(flask))
+
+import os
+
+# print(os.getcwd())
+# os.mkdir("Tilak")
+# os.chdir("D:\Cadd Mentor\Student_Class_Code")
+# print(os.getcwd())
+
+# print(os.listdir())
+
+# os.rename("Rohan","Rohani")
+# os.remove("Rohani")
+# os.rmdir("Rohani")
+
+# print(os.environ)
+
